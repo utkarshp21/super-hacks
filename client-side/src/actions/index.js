6bbb21d5a00d9ec459a4ad451cbc7a0344842aed
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2018-11-10 21:35:04 -0500

    access token

--- client-side/src/actions/index.js
+++ client-side/src/actions/index.js
@@ -1,5 +1,6 @@
 
 export const RECIEVE_ALL_EVENTS = 'RECIEVE_ALL_EVENTS';
+export const RECIEVE_ACCESS_TOKEN = 'RECIEVE_ACCESS_TOKEN';
 
 
 export function receiveEvents(events) {
@@ -29,5 +30,35 @@ export function fetchEvents() {
         //         }       
         // });
     };
+}
+
+export function recieveAccessToken(token){
+     return {
+         type: RECIEVE_ACCESS_TOKEN,
+         token
+     };
+}
+
+export function getSpotifyDetails() {
+    return (dispatch, getState) => {
+        const token = {
+            "access_token": "BQChpNJQlUsNxwR_79e-q6qd6lIujU78E304yFRpwHpEjlK8LZIO12eAunB4SSdzoi-FCVtPITh-FbfPRhZackI1YX_lcbiilEi0eYAk8d2xchY5j03hAbbtriCs7juanXYvhcC1rIQ-cDkmGjqCEHz4744OMeaSFZ1hjJT7qBlGWpWiQzxzjD1kTqkhwwcTRyye6wE",
+            "refresh_token": "AQAt9sEezH1MxmKrTxwFNfX3zWAXS85oGpoQip4FVckS9mUPhRTuf8I19VbfxXa8bvrk92-owz08D6JqBBU9NsztKZNdSlVxOEBOLPUgc0k6imD7j3Zahb1EFPIrrtpL2QJxCQ"
+        }
+        dispatch(recieveAccessToken(token))
+    
+        // return (dispatch, getState) => {
+    //     return fetch("http://15b58e96.ngrok.io/login", {
+    //             method: 'GET',
+    //         })
+    //         .then((response) => {debugger;response.json()})
+    //         .then(json => {
+    //             debugger;
+    //             // dispatch(receiveEvents(json))
+                   
+    //     });
+    // };
+    }
+
 }
 
