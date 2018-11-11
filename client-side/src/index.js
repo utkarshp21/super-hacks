a2e0f7c11c044d79c375a3158ccdbc6413e4c00d
Utkarsh Prakash <monsieurutkarsh@gmail.com>
2018-11-11 01:27:53 -0500

    spotify connect

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -15,9 +15,7 @@ const store = configureStore()
 ReactDOM.render((
   <Provider store={store}>
   	<BrowserRouter>
-    
       <App />
- 	
  	</BrowserRouter>
   </Provider>
 ), document.getElementById('root'))
